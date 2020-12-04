bddd743354a6eb30b2342d7366223a0d011f1794
Nir Chodorov <nirhodorov@gmail.com>
2020-12-04 10:19:18 +0200

    Server port set on application start

--- src/main/java/com/ebaytask/salesstatistics/SalesstatisticsApplication.java
+++ src/main/java/com/ebaytask/salesstatistics/SalesstatisticsApplication.java
@@ -3,11 +3,16 @@ package com.ebaytask.salesstatistics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class SalesstatisticsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SalesstatisticsApplication.class, args);
+        SpringApplication app = new SpringApplication(SalesstatisticsApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
     }
 
 }
